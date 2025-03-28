e7ae5c68e91bae8b492a8191031e9fb1df6dd8b9
JasperMunene <jaspermunene2022@gmail.com>
2025-03-28 12:10:32 +0300

    Fix: Add timeout to send sms helper

--- utils/send_sms.py
+++ utils/send_sms.py
@@ -1,48 +1,45 @@
 import requests
+import os
+
 
 class SendSms:
     def __init__(self, phone, message):
-        self.phone = phone
+        self.phone = str(phone)
         self.message = message
-        
+
     def post(self):
-        message = self.message
-        phone = self.phone
-        # Remove leading zero from phone number
-        formatted_phone = phone.lstrip('0')
-        
+        # Remove leading zero(s) from phone number
+        formatted_phone = self.phone.lstrip('0')
+        full_phone = f'254{formatted_phone}'
+
         # URL for SMS portal
         url = "https://smsportal.hostpinnacle.co.ke/SMSApi/send"
-        
+
         # Prepare payload
         payload = {
             'userid': 'mealscok',
             'password': '-cCtu@NY39w5L8',
-            'mobile': f'254{formatted_phone}',
-            'msg': message,
+            'mobile': full_phone,
+            'msg': self.message,
             'senderid': 'Intacom',
             'msgType': 'text',
             'duplicatecheck': 'true',
             'output': 'json',
             'sendMethod': 'quick'
         }
-        
+
         # Headers
         headers = {
-            'apikey': '58314795da56adcdaae1c219fd48fbf35229e7d5',
+            'apikey': os.getenv("SMS_API_KEY"),
             'cache-control': 'no-cache',
             'content-type': 'application/x-www-form-urlencoded'
         }
-        
+
         try:
-            # Send POST request
-            response = requests.post(url, data=payload, headers=headers)
-            
-            # Check if request was successful
-            if response.status_code == 200:
-                return {"success": "success"}
-            else:
-                return {"error": f"HTTP error {response.status_code}: {response.text}"}
-        
+            response = requests.post(url, data=payload, headers=headers, timeout=10)
+            response.raise_for_status()
+            return {"success": "success"}
+        except requests.HTTPError as http_err:
+            return {"error": f"HTTP error occurred: {http_err}"}
         except requests.RequestException as e:
             return {"error": str(e)}
