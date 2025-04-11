34661daac6d07493d08904808343aaf804c8c8e8
mashfied <victoreslieh@gmail.com>
2025-04-11 19:34:48 +0300

    feat: add important sms notifications to user

--- utils/send_notification.py
+++ utils/send_notification.py
@@ -1,18 +1,23 @@
-from flask import current_app, request
+from flask import current_app
 from extensions import socketio
 from models import db
 from models import Notification
+from utils.send_sms import SendSms
+
 class Notify():
-    def __init__(self, user_id, message, source):
+    def __init__(self, user_id, message, source, is_important=False):
         self.user_id = str(user_id)
         self.message = message
         self.source = source
+        self.is_important = is_important
         
     def post(self):
         user_id = self.user_id
         message = self.message
         source = self.source
+        is_important = self.is_important
 
+        # Save notification
         notification = Notification(
             user_id=user_id,
             message=message,
@@ -23,11 +28,9 @@ class Notify():
         db.session.commit()
         
         with current_app.app_context():
-        # Check if receiver is online
+            # Check if user is online
             receiver_sid = current_app.cache.get(f"user_sid:{user_id}")
             if receiver_sid:
-                # Update message status to delivered
-
                 # Emit to receiver
                 socketio.emit('new_sms', {
                     'notification_id': notification.id,
@@ -38,6 +41,18 @@ class Notify():
                 print(f"notification sent to user {user_id} connected to {receiver_sid}")
             else:
                 print(f"user {user_id} not online to be notified")
-                
 
-        
+            # Send SMS if important
+            if is_important:
+                phone_number = self._get_user_phone(user_id)
+                if phone_number:
+                    SendSms(phone_number, message).post()
+                    print(f"Important notification: SMS sent to {phone_number}")
+                else:
+                    print("Phone number not found for user")
+
+    def _get_user_phone(self, user_id):
+        # Lazy import to avoid circular dependencies
+        from models import User
+        user = User.query.filter_by(id=user_id).first()
+        return user.phone if user else None
